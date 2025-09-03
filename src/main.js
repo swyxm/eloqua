dfd650c3f9c80f8f71cc14df78d8d66200df0bac
swyxm <swayamparekh1276@gmail.com>
2025-09-02 23:02:01 -0700

    chore: updated main

--- src/main.js
+++ src/main.js
@@ -10,12 +10,14 @@ import Upload from './renderer/views/Upload.vue';
 import Speeches from './renderer/views/Speeches.vue';
 import SpeechDetail from './renderer/views/SpeechDetail.vue';
 import Stats from './renderer/views/Stats.vue';
+import DataIngest from './renderer/views/DataIngest.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Upload },
   { path: '/speeches', name: 'Speeches', component: Speeches },
   { path: '/speech/:id', name: 'SpeechDetail', component: SpeechDetail },
-  { path: '/stats', name: 'Stats', component: Stats }
+  { path: '/stats', name: 'Stats', component: Stats },
+  { path: '/ingest', name: 'DataIngest', component: DataIngest }
 ]
 
 const history = import.meta.env.MODE === 'development'
